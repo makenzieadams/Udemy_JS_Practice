cc46af8cec10888a70515dc34135803ced2cc65f
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-07 22:30:06 -0500

    temp

---
+++ temp.js
@@ -0,0 +1,13 @@
+let far = 28; // 0c -273.15kelvin
+
+// calculate celsius and store in celcius varibale
+//print that variable
+
+// calculate kelvin and store in kelvin varibale
+//print that variable
+
+let cel = ((far - 32) * 5) / 9;
+console.log(cel);
+
+let kel = ((far + 459.67) * 5) / 9;
+console.log(kel);
