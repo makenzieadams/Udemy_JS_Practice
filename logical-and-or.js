1fbabe78757c4deef257f38367fa23a2edc0c130
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-08 17:31:24 -0500

    vegan

--- logical-and-or.js
+++ logical-and-or.js
@@ -1,4 +1,4 @@
-let temp = 70;
+/*let temp = 70;
 
 if (temp >= 60 && temp <= 90) {
   console.log("It is pretty nice out");
@@ -6,4 +6,25 @@ if (temp >= 60 && temp <= 90) {
 
 if (temp <= 0 || temp >= 120) {
   console.log("Do not go outside");
+} */
+
+//Same as:
+
+/*if (temp >= 60 && temp <= 90) {
+    console.log('It is pretty nice out')
+} else if (temp <= 0 || temp >= 120) {
+
+} else {
+    console.log('Eh. Do what you want')
+}*/
+
+let firstGuestVegan = false;
+let secondGuestVegan = false;
+
+if (firstGuestVegan && secondGuestVegan) {
+  console.log("Vegan dishes only");
+} else if (firstGuestVegan || secondGuestVegan) {
+  console.log("Offer some vegan");
+} else {
+  console.log("Give regular menu");
 }
