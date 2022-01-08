52299b796a5318f5304a89a8891ccb6893f07b1d
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-07 21:57:36 -0500

    numbers

---
+++ numbers.js
@@ -0,0 +1,5 @@
+let studentScore = 25;
+let maxScore = 30;
+let percent = (studentScore / maxScore) * 100;
+
+console.log(percent);
---
+++ strings.js
@@ -0,0 +1,5 @@
+let city = "Salisbury";
+let country = "USA";
+let location = city + ", " + country;
+
+console.log(location);
