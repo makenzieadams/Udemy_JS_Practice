2c34738220e3b46b81c0d9c16fc02e6e1bf85be4
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-10 00:08:42 -0500

    arguments2

--- arguments.js
+++ arguments.js
@@ -1,6 +1,21 @@
-let add = function (a, b, c) {
+/*let add = function (a, b, c) {
   return a + b + c;
 };
 
 let result = add(10, 1, 5);
-console.log(result);
+console.log(result);*/
+
+/*let getScoreText = function (name = "Ann", score = 0) {
+  return "Name: " + name + " - Score: " + score;
+};
+let scoreText = getScoreText(undefined, 50);
+console.log(scoreText);*/
+
+//CHALLENGE
+
+let getTip = function (total, tipPercent = 0.2) {
+  return total * tipPercent;
+};
+
+let tipAmount = getTip(10, 0.15);
+console.log(tipAmount);
