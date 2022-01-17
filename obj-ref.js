f2e6c2be1d86df95faa2906c94f8dbbdd401c380
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-16 22:23:28 -0500

    expenseaccount

--- obj-ref.js
+++ obj-ref.js
@@ -3,3 +3,17 @@ let myAccount = {
   expenses: 0,
   income: 0,
 };
+
+let otherAccount = myAccount;
+otherAccount.income = 1000;
+
+// passing around objects
+// function that allows us to add expense
+
+let addExpense = function (account, amount) {
+  account.expenses = account.expenses + amount;
+  console.log(account);
+};
+
+addExpense(myAccount, 2.5);
+console.log(myAccount);
