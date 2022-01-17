17431cb088600acfb84e280223e6cf2c901fc96e
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-16 23:17:36 -0500

    methodstwo

--- obj-methods.js
+++ obj-methods.js
@@ -1,13 +1,23 @@
 let restaurant = {
   name: "TRH",
   guestCapacity: 75,
-  guestCount: 73,
+  guestCount: 0,
   checkAv: function (partySize) {
     let seatsLeft = this.guestCapacity - this.guestCount;
     return partySize <= seatsLeft;
   },
+  seatParty: function (partySize) {
+    this.guestCount = this.guestCount + partySize;
+  },
+  removeParty: function (partySize) {
+    this.guestCount = this.guestCount - partySize;
+  },
 };
 
-let status = restaurant.checkAv(4);
+//seatParty
+//removeParty
 
-console.log(status);
+restaurant.seatParty(72);
+console.log(restaurant.checkAv(4));
+restaurant.removeParty(5);
+console.log(restaurant.checkAv(4));
