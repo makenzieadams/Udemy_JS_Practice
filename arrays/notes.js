b6aa193ada799040d4f85b5b8a3394d6d08bea53
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-17 22:36:15 -0500

    todochallenge

--- arrays/notes.js
+++ arrays/notes.js
@@ -6,7 +6,9 @@ const notes = ["Note 1", "Note 2", "Note 3"];
 // console.log(notes.shift());
 // notes.unshift("My first note");
 
-notes.splice(1, 1, "This is the new second item");
+// notes.splice(1, 1, "This is the new second item");
+
+notes[2] = "This is now the new note 3";
 
 console.log(notes.length);
 console.log(notes);
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,8 +1,26 @@
+//Challenge 1
+
 // Create an array with 5 "todos"
 // You have 'x' todos - msg - 'x' lists the length of the array
 // Print 1st and 2nd to last items
 
+// const todos = ["Bath", "Brush Teeth", "Dry Hair", "Paint Nails", "Makeup"];
+
+// console.log(`You have ${todos.length} todos.`);
+// console.log(`Todo: ${todos[4]}, and ${todos[3]}.`);
+
+//******************************************************** */
+
+//Challenge 2
+//Delete 3rd item
+//Add new item to end
+//Remove 1st item from list
+
 const todos = ["Bath", "Brush Teeth", "Dry Hair", "Paint Nails", "Makeup"];
 
+todos.splice(2, 1);
+todos.push("Go to Bed");
+todos.shift();
+
 console.log(`You have ${todos.length} todos.`);
-console.log(`Todo: ${todos[4]}, and ${todos[3]}.`);
+console.log(todos);
