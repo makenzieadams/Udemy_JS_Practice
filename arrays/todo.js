ba4dc60c4fe9e164625c6d1c2d3a4d5e79a6c50f
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-17 22:15:20 -0500

    todolist

---
+++ arrays/todo.js
@@ -0,0 +1,8 @@
+// Create an array with 5 "todos"
+// You have 'x' todos - msg - 'x' lists the length of the array
+// Print 1st and 2nd to last items
+
+const todos = ["Bath", "Brush Teeth", "Dry Hair", "Paint Nails", "Makeup"];
+
+console.log(`You have ${todos.length} todos.`);
+console.log(`Todo: ${todos[4]}, and ${todos[3]}.`);
