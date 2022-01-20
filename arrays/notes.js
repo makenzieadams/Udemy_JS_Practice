29f29331782ba6fb82b7e4107f8cb666ff34a1f0
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-19 23:47:46 -0500

    search

--- arrays/notes.js
+++ arrays/notes.js
@@ -8,12 +8,17 @@ const notes = ["Note 1", "Note 2", "Note 3"];
 
 // notes.splice(1, 1, "This is the new second item");
 
-notes[2] = "This is now the new note 3";
+// notes[2] = "This is now the new note 3";
 
-notes.forEach(function (item, index) {
-  console.log(index);
-  console.log(item);
-});
+// notes.forEach(function (item, index) {
+//   console.log(index);
+//   console.log(item);
+// });
 
 console.log(notes.length);
 console.log(notes);
+
+File.arguments(function({
+    return true
+}))
+console.log
