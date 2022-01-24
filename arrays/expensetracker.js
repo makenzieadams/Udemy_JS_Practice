e8d048371901deb7928aaafe9a9b4daab9f9c84c
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-23 19:50:50 -0500

    expensetrackertwo

--- arrays/expensetracker.js
+++ arrays/expensetracker.js
@@ -3,6 +3,10 @@ const account = {
   addExpense: function (description, amount) {
     this.expenses.push({ description, amount });
   },
+  addIncome: function (description, amount) {
+    this.income.push({ description, amount });
+  },
+  income: [],
   expenses: [
     {
       description: "Rent",
@@ -26,20 +30,34 @@ const account = {
     },
   ],
   getAccountSummary: function () {
-    let total = 0;
+    let totalExpenses = 0;
     this.expenses.forEach(function (expense) {
-      total = expense.amount + total;
+      totalExpenses = expense.amount + totalExpenses;
     });
-    return `${this.name} has $${total} in expenses.`;
+    let totalIncome = 0;
+    this.income.forEach(function (income) {
+      totalIncome = income.amount + totalIncome;
+    });
+    return `${this.name} has a balance of $${
+      totalIncome - totalExpenses
+    }. $${totalIncome} in income, and $${totalExpenses} in expenses.`;
   },
 };
 
 account.addExpense("Hair", 150);
 account.addExpense("groceries", 150);
+account.addIncome("Job", 2000);
 
 console.log(account.getAccountSummary());
+
+//Part One
 
 //Expense -> description (string), amount
 //addExpense -> 2 arguments: descriptions, amount
 //getAccountSummary -> total up all expenses -> return string
 //use forEach to go through every item in expenses array - get object and add it onto variable
+
+//Part Two
+// 1. add income array to account
+// 2. add income method -> description, amount
+// 3. tweak getAccountSummary
