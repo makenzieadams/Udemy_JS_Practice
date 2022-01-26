0e861c2678189f6565b2da8447c0564a2959a8ed
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-25 23:02:01 -0500

    challenge3

--- todo-app/challenge-1.js
+++ todo-app/challenge-1.js
@@ -1,3 +1,18 @@
+const notes = [
+  {
+    title: "My next trip",
+    body: "I would like to go to Spain",
+  },
+  {
+    title: "Habbits to work on",
+    body: "Exercise. Eating a bit better.",
+  },
+  {
+    title: "Office modification",
+    body: "Get a new seat",
+  },
+];
+
 const the = document.querySelectorAll("p");
 the.forEach(function (p) {
   if (p.textContent.includes("THE")) {
---
+++ todo-app/challenge-2.js
@@ -0,0 +1,35 @@
+const notes = [
+  {
+    text: "order food",
+    completed: false,
+  },
+  {
+    text: "clean house",
+    completed: true,
+  },
+  {
+    text: "walk dog",
+    completed: false,
+  },
+];
+
+// const the = document.querySelectorAll("p");
+// the.forEach(function (p) {
+//   if (p.textContent.includes("THE")) {
+//     p.remove();
+//   }
+// });
+
+const incompleteTodos = todos.filter(function (todo) {
+  return !todo.completed;
+});
+
+const summary = document.createElement("p");
+summary.textContent = `You have ${incompleteTodos.length} todos left`;
+document.querySelector("body").appendChild(summary);
+
+todos.forEach(function (todo) {
+  const p = document.createElement("p");
+  p.textContent = todo.text;
+  document.querySelector("body").appendChild(p);
+});
