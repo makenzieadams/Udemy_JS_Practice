66696bc804b6cccb3e1c90d1650bb1ecb5614dfb
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-27 18:56:13 -0500

    notes

--- NOTESLEARNING/notes-app/notes-app.js
+++ NOTESLEARNING/notes-app/notes-app.js
@@ -1,14 +1,18 @@
-// Dom - Document Object Model (object in JS that models the doc)
-//Query and remove
+const notes = [
+  {
+    title: "My next trip",
+    body: "I would like to go to Spain",
+  },
+  {
+    title: "Habbits to work on",
+    body: "Exercise. Eating a bit better.",
+  },
+  {
+    title: "Office modification",
+    body: "Get a new seat",
+  },
+];
 
-// const p = document.querySelector("h2");
-// p.remove();
-
-//Query all and remove
-
-const ps = document.querySelectorAll("p");
-ps.forEach(function (p) {
-  p.textContent = "*******";
-  //   console.log(p.textContent);
-  //   p.remove();
+document.querySelector("button").addEventListener("Click", function (e) {
+  e.target.textContent = "The button was clicked";
 });
