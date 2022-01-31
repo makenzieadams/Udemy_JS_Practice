5995fd7f7f490a22c2f6c758d5ca8866f26d57e3
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-30 21:12:37 -0500

    location

--- JS-basics/strings.js
+++ JS-basics/strings.js
@@ -1,4 +1,12 @@
 let firstName = "Makenzie";
 let lastName = "Adams";
+let fullName = firstName + " " + lastName;
 
-console.log(firstName, lastName);
+console.log(fullName);
+
+let city = "Salisbury";
+let state = "North Carolina";
+let country = "United States";
+let location = city + " " + state + " " + country;
+
+console.log(location);
