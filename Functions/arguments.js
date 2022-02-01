3ab6931ab0a6a558758e9ff071cb53d41064f7ca
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-01 13:47:43 -0500

    tipchallenge

--- Functions/arguments.js
+++ Functions/arguments.js
@@ -1,6 +1,22 @@
-let add = function (a, b, c) {
-  return a + b + c;
-};
+// let add = function (a, b, c) {
+//   return a + b + c;
+// };
 
-let result = add(10, 1, 5);
-console.log(result);
+// let result = add(10, 1, 5);
+// console.log(result);
+
+// let getScoreText = function (name = "Anonymous", score = 0) {
+//   return "Name: " + name + " -Score " + score;
+// };
+// let scoreText = getScoreText(undefined, 50);
+// console.log(scoreText);
+
+let tip = function (total = 20, tipPercent = 0.2) {
+  //   let getTip = total * tipPercent;
+  //   let grandTotal = getTip + total;
+  //   return grandTotal;
+  let grandTotal = total * tipPercent + total;
+  return grandTotal;
+};
+let tipAmount = tip(undefined, 0.25);
+console.log(tipAmount);
