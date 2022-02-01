ce1b83bb300b1b767869bfa6316a638b6fa493ed
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-01 13:01:52 -0500

    undefined

---
+++ Functions/undefined-null.js
@@ -0,0 +1,20 @@
+let name = "Jen";
+
+if (name === undefined) {
+  console.log("Please provide a name");
+} else {
+  console.log(name);
+}
+
+let square = function (num) {
+  console.log(num);
+};
+let result = square();
+
+console.log(result);
+
+let age = 27;
+
+age = undefined;
+
+console.log(age);
