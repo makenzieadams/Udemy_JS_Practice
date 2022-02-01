96bbba157c50d706ca50844b1f725462814502d0
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-01 12:09:21 -0500

    functionchallenge1

--- Functions/functions-101.js
+++ Functions/functions-101.js
@@ -1,13 +1,21 @@
-let greetUser = function () {
-  console.log("Welcome User");
+// let greetUser = function () {
+//   console.log("Welcome User");
+// };
+
+// greetUser();
+
+// let square = function (num) {
+//   let result = num * num;
+//   return result;
+// };
+
+// let value = square(3);
+// console.log(value);
+
+let convertFarenheitToCelsius = function (far) {
+  return ((far - 32) * 5) / 9;
 };
 
-greetUser();
-
-let square = function (num) {
-  let result = num * num;
-  return result;
-};
-
-let value = square(3);
-console.log(value);
+console.log(convertFarenheitToCelsius(32));
+console.log(convertFarenheitToCelsius(68));
+console.log(convertFarenheitToCelsius(75));
