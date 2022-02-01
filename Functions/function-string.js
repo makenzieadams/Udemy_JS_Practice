1d04d0d6cecb2de20a27fbb7db44505a3f7413fb
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-01 14:46:13 -0500

    functionstringchallenge

--- Functions/function-string.js
+++ Functions/function-string.js
@@ -1,21 +1,20 @@
-let add = function (a, b, c) {
-  return a + b + c;
-};
+// let add = function (a, b, c) {
+//   return a + b + c;
+// };
 
-let result = add(10, 1, 5);
-console.log(result);
+// let result = add(10, 1, 5);
+// console.log(result);
 
-let getScoreText = function (name = "Anonymous", score = 0) {
-  return "Name: " + name + " -Score " + score;
-};
-let scoreText = getScoreText(undefined, 50);
-console.log(scoreText);
+// let getScoreText = function (name = "Anonymous", score = 0) {
+//   return "Name: " + name + " -Score " + score;
+// };
+// let scoreText = getScoreText(undefined, 50);
+// console.log(scoreText);
 
-let tip = function (total = 20, tipPercent = 0.2) {
-  return total * tipPercent;
+let getTip = function (total, tipPercent) {
+  let percent = tipPercent * 100;
+  let tipAmount = total * tipPercent;
+  return `A ${percent}% tip on $${total} would be $${tipAmount}.`;
 };
-let tipAmount = tip(40, 0.25);
+let tipAmount = getTip(25, 0.2);
 console.log(tipAmount);
-
-let name = "Brandon";
-console.log(`Hey, my name is ${name}!`);
