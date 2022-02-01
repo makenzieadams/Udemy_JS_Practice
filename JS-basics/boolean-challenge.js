e12b11d66f0c316a10e088a14c5fa5d5f3017d23
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-31 23:32:12 -0500

    elseifchallenge

---
+++ JS-basics/boolean-challenge.js
@@ -0,0 +1,9 @@
+let temp = 55;
+
+if (temp <= 45) {
+  console.log("It is freezing outside");
+} else if (temp >= 65) {
+  console.log("It is hot outside");
+} else {
+  console.log("Go for it, it is pretty nice");
+}
