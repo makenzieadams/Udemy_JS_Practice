e30f7a30ba98d936516536cdc27e37b2ab1e5e3a
Makenzie Adams <adamsm.realtor@gmail.com>
2022-01-31 23:04:34 -0500

    park

--- JS-basics/booleans.js
+++ JS-basics/booleans.js
@@ -1,4 +1,11 @@
-let temp = 31;
-let isFreezing = temp >= 28;
+// let temp = 31;
+// let isFreezing = temp >= 28;
 
-console.log(isFreezing);
+// console.log(isFreezing);
+
+let age = 66;
+let child = age <= 7;
+let senior = age >= 65;
+
+console.log(child);
+console.log(senior);
