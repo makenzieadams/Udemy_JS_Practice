3239bb0f6b20f0a78daf99c89affaaebb68dd463
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-03 15:37:46 -0500

    expensechallenge

--- Objects/objects-ref.js
+++ Objects/objects-ref.js
@@ -4,12 +4,24 @@ let myAccount = {
   income: 0,
 };
 
-let otherAccount = myAccount;
-otherAccount.income = 1000;
-
 let addExpense = function (account, amount) {
   account.expenses = account.expenses + amount;
 };
+let addIncome = function (account, income) {
+  account.income = account.income + income;
+};
+let resetAccount = function (account) {
+  account.expenses = 0;
+  account.income = 0;
+};
+let getAccountSummary = function (account) {
+  let accountBalance = account.income - account.expenses;
+  return `Account for ${account.name} has ${accountBalance}. $${account.income} in income, and $${account.expenses} in expenses.`;
+};
 
-addExpense(myAccount, 2.5);
-console.log(myAccount);
+addIncome(myAccount, 2000);
+addExpense(myAccount, 250);
+addExpense(myAccount, 500);
+console.log(getAccountSummary(myAccount));
+resetAccount(myAccount);
+console.log(getAccountSummary(myAccount));
