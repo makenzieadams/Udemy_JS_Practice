c10e6a8b2e2f8f63192fe003893094633d86b4e0
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-03 15:17:51 -0500

    objectref

---
+++ Objects/objects-ref.js
@@ -0,0 +1,15 @@
+let myAccount = {
+  name: "Makenzie",
+  expenses: 0,
+  income: 0,
+};
+
+let otherAccount = myAccount;
+otherAccount.income = 1000;
+
+let addExpense = function (account, amount) {
+  account.expenses = account.expenses + amount;
+};
+
+addExpense(myAccount, 2.5);
+console.log(myAccount);
