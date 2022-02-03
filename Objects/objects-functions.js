2d1e7bf65ddbf6557c0d2d49788006377afe9e9c
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-03 15:06:50 -0500

    objectfunctionschallenge2

--- Objects/objects-functions.js
+++ Objects/objects-functions.js
@@ -11,8 +11,28 @@ let otherBook = {
 };
 
 let getSummary = function (book) {
-  console.log(`${book.title}, by ${book.author}`);
+  return {
+    summary: `${book.title}, by ${book.author}`,
+    pageCountSummary: `${book.title} is ${book.pageCount} pages long.`,
+  };
 };
 
-getSummary(myBook);
-getSummary(otherBook);
+let bookSummary = getSummary(myBook);
+let otherBookSummary = getSummary(otherBook);
+
+console.log(bookSummary.pageCountSummary);
+
+//Challenge
+//convert far to celsius and kelvin
+//create function - take far in, return object with all three
+
+let conversions = function (far) {
+  return {
+    farenheit: far,
+    celsius: ((far - 32) * 5) / 9,
+    kelvin: ((far + 459.67) * 5) / 9,
+  };
+};
+
+let temps = conversions(74);
+console.log(temps);
