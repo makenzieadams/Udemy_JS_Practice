e5590064bc09ab578688c672c2a33b6729875e85
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-03 22:47:45 -0500

    math2

--- Objects/number-methods.js
+++ Objects/number-methods.js
@@ -6,3 +6,8 @@ console.log(Math.round(num));
 console.log(Math.floor(num));
 console.log(Math.ceil(num));
 console.log(Math.random(num));
+
+let min = 10;
+let max = 20;
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(randomNum);
