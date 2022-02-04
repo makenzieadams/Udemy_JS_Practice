b62da869c7ca9a05ff4306fb62fd7171c5de9048
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-04 17:08:18 -0500

    todochallenge1

---
+++ Arrays/todo.js
@@ -0,0 +1,11 @@
+const todos = [
+  "Walk the dog",
+  "Buy food",
+  "Groom dog",
+  "Feed dog",
+  "Take a walk",
+];
+
+console.log(`You have ${todos.length} things to do.`);
+
+console.log(`You need to: ${todos[0]} and: ${todos[todos.length - 2]}`);
