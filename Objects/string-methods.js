c5f58a48e830336d81f675fd939381a578f54531
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-03 22:30:02 -0500

    methodstringschallenge

--- Objects/string-methods.js
+++ Objects/string-methods.js
@@ -1,9 +1,19 @@
-let name = "   Makenzie Adams ";
-console.log(name.length);
+// let name = "   Makenzie Adams ";
+// console.log(name.length);
 
-console.log(name.toLowerCase());
+// console.log(name.toLowerCase());
 
-let password = "abc123";
-console.log(password.includes("123"));
+// let password = "abc123";
+// console.log(password.includes("123"));
 
-console.log(name.trim());
+// console.log(name.trim());
+
+let isValidPassword = function (password) {
+  if (password.length >= 8 && !password.includes("password")) {
+    return true;
+  }
+
+  return false;
+};
+
+console.log(isValidPassword("password"));
