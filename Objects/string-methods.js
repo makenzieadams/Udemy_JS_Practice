4f124150c755f773a8a143914273be534427a2b3
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-03 22:11:16 -0500

    methodstrings

---
+++ Objects/string-methods.js
@@ -0,0 +1,9 @@
+let name = "   Makenzie Adams ";
+console.log(name.length);
+
+console.log(name.toLowerCase());
+
+let password = "abc123";
+console.log(password.includes("123"));
+
+console.log(name.trim());
