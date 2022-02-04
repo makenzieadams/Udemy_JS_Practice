84bb32ddfbcb29935c321d2a847b103bb9dde17d
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-03 21:54:42 -0500

    methodschallenge

--- Objects/objects-methods.js
+++ Objects/objects-methods.js
@@ -1,13 +1,20 @@
 let restaurant = {
   name: "Olive Garden",
   guestCapacity: 75,
-  guestCount: 73,
+  guestCount: 0,
   checkAvailability: function (partySize) {
     let seatsLeft = this.guestCapacity - this.guestCount;
     return partySize <= seatsLeft;
   },
+  seatParty: function (partySize) {
+    this.guestCount = this.guestCount + partySize;
+  },
+  removeParty: function (partySize) {
+    this.guestCount = this.guestCount - partySize;
+  },
 };
 
-let staus = restaurant.checkAvailability(4);
-
-console.log(staus);
+restaurant.seatParty(55);
+console.log(restaurant.checkAvailability(4));
+restaurant.removeParty(5);
+console.log(restaurant.checkAvailability(4));
