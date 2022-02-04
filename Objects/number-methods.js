fda5a6704153685dd829535a18a17cf57d85504c
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-03 23:37:25 -0500

    numbermethodchallenge

--- Objects/number-methods.js
+++ Objects/number-methods.js
@@ -1,13 +1,23 @@
-let num = 123.456;
+// let num = 123.456;
 
-console.log(num.toFixed(2));
+// console.log(num.toFixed(2));
 
-console.log(Math.round(num));
-console.log(Math.floor(num));
-console.log(Math.ceil(num));
-console.log(Math.random(num));
+// console.log(Math.round(num));
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+// console.log(Math.random(num));
 
-let min = 10;
-let max = 20;
-let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-console.log(randomNum);
+// let min = 0;
+// let max = 1;
+// let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+
+let makeGuess = function (guess) {
+  let min = 1;
+  let max = 5;
+  const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+  if (randomNum === guess) {
+    return true;
+  }
+  return false;
+};
+console.log(makeGuess(5));
