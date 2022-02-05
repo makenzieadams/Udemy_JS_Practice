c48b74dade8d4e965f24a4e8803394c63234d382
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-04 23:08:33 -0500

    forstatement

--- Arrays/notes.js
+++ Arrays/notes.js
@@ -22,6 +22,20 @@
 
 //************************************************************ */
 
+// const notes = ["Note 1", "Note 2", "Note 3"];
+
+// notes.forEach(function (item, index) {
+//   console.log(index);
+//   console.log(item);
+// });
+
+// notes[2] = "This is now the new note three";
+
+// console.log(notes.length);
+// console.log(notes);
+
+//************************************************************** */
+
 const notes = ["Note 1", "Note 2", "Note 3"];
 
 notes.forEach(function (item, index) {
@@ -33,3 +47,11 @@ notes[2] = "This is now the new note three";
 
 console.log(notes.length);
 console.log(notes);
+
+for (let count = 2; count >= 0; count--) {
+  console.log(count);
+}
+
+for (let count = 0; count < notes.length; count++) {
+  console.log(notes[count]);
+}
