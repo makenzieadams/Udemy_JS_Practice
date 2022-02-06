05c4783969fb3ae70251f467baf9da30fe808e24
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-06 12:52:46 -0500

    indexof

--- Arrays/notes.js
+++ Arrays/notes.js
@@ -36,22 +36,56 @@
 
 //************************************************************** */
 
-const notes = ["Note 1", "Note 2", "Note 3"];
+// const notes = ["Note 1", "Note 2", "Note 3"];
 
-notes.forEach(function (item, index) {
-  console.log(index);
-  console.log(item);
-});
+// notes.forEach(function (item, index) {
+//   console.log(index);
+//   console.log(item);
+// });
 
-notes[2] = "This is now the new note three";
+// notes[2] = "This is now the new note three";
+
+// console.log(notes.length);
+// console.log(notes);
+
+// for (let count = 2; count >= 0; count--) {
+//   console.log(count);
+// }
+
+// for (let count = 2; count < notes.length; count++) {
+//   console.log(notes[count]);
+// }
+
+// for (let count = notes.length - 1; count >= 0; count--) {
+//     console.log(notes[count]);
+//   }
+
+//********************************************************** */
+
+const notes = [
+  {},
+  {
+    title: "My next trip",
+    body: "I would like to go to Spain",
+  },
+  {
+    title: "Habits to work on",
+    body: "Work out",
+  },
+  {
+    title: "Office modifications",
+    body: "Get a new seat",
+  },
+];
+
+// notes.forEach(function (item, index) {
+//   console.log(index);
+//   console.log(item);
+// });
+
+// notes[2] = "This is now the new note three";
 
 console.log(notes.length);
 console.log(notes);
 
-for (let count = 2; count >= 0; count--) {
-  console.log(count);
-}
-
-for (let count = notes.length - 1; count >= 0; count--) {
-  console.log(notes[count]);
-}
+console.log({} === {});
--- Arrays/todo.js
+++ Arrays/todo.js
@@ -15,9 +15,3 @@ console.log(`You have ${todos.length} things to do.`);
 todos.forEach(function (todo, index) {
   console.log(`${index + 1}. ${todo}`);
 });
-
-for (let todo = 0; todo < todos.length; todo++) {
-  const num = todo + 1;
-  const count = todos[todo];
-  console.log(`${num}. ${count}`);
-}
