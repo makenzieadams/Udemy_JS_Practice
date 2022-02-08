be32c95fc06d917f94e6c156f271b7341e02a2cf
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-07 22:49:12 -0500

    sort

--- Arrays/notes.js
+++ Arrays/notes.js
@@ -141,6 +141,42 @@
 
 //****************************************************************** */
 
+// const notes = [
+//   {
+//     title: "My next trip",
+//     body: "I would like to go to Spain",
+//   },
+//   {
+//     title: "Habits to work on",
+//     body: "Work out",
+//   },
+//   {
+//     title: "Office modifications",
+//     body: "Get a new seat",
+//   },
+// ];
+
+// const findNote = function (notes, noteTitle) {
+//   return notes.find(function (note, index) {
+//     return note.title.toLowerCase() === noteTitle.toLowerCase();
+//   });
+// };
+
+// const findNotes = function (notes, query) {
+//   return notes.filter(function (note, index) {
+//     const isTitleMatch = note.title.toLowerCase().includes(query.toLowerCase());
+//     const isBodyMatch = note.body.toLowerCase().includes(query.toLowerCase());
+//     return isTitleMatch || isBodyMatch;
+//   });
+// };
+
+// console.log(findNotes(notes, "work"));
+
+// const note = findNote(notes, "office modifications");
+// console.log(note);
+
+//********************************************************************* */
+
 const notes = [
   {
     title: "My next trip",
@@ -156,6 +192,18 @@ const notes = [
   },
 ];
 
+const sortNotes = function (notes) {
+  notes.sort(function (a, b) {
+    if (a.title.toLowerCase() < b.title.toLowerCase()) {
+      return -1;
+    } else if (b.title.toLowerCase() < a.title.toLowerCase()) {
+      return 1;
+    } else {
+      0;
+    }
+  });
+};
+
 const findNote = function (notes, noteTitle) {
   return notes.find(function (note, index) {
     return note.title.toLowerCase() === noteTitle.toLowerCase();
@@ -170,7 +218,10 @@ const findNotes = function (notes, query) {
   });
 };
 
-console.log(findNotes(notes, "work"));
+// console.log(findNotes(notes, "work"));
 
-const note = findNote(notes, "office modifications");
-console.log(note);
+// const note = findNote(notes, "office modifications");
+// console.log(note);
+
+sortNotes(notes);
+console.log(notes);
