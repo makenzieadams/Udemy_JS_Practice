bb39765618cae01051c437676fc1686818cdd396
Makenzie Adams <adamsm.realtor@gmail.com>
2022-02-24 18:12:48 -0500

    mybook

--- Objects/objects-101.js
+++ Objects/objects-101.js
@@ -3,28 +3,5 @@ let myBook = {
   author: "George Orwell",
   pageCount: 326,
 };
-console.log(
-  `${myBook.title} by ${myBook.author} has ${myBook.pageCount} pages.`
-);
 
-myBook.title = "Animal Farm";
-console.log(
-  `${myBook.title} by ${myBook.author} has ${myBook.pageCount} pages.`
-);
-
-//Challenge
-//name, age, location
-//create object and create string based on the properties
-//increase age by 1 and print again
-
-let myPerson = {
-  name: "Makenize",
-  age: 27,
-  location: "North Carolina",
-};
-
-console.log(
-  `My name is ${myPerson.name}. I am ${myPerson.age + 1}, and I live in ${
-    myPerson.location
-  }.`
-);
+console.log(myBook);
