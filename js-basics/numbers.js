ccb636592c5e7185bcc50554e17401b1a8f82b1f
Makenzie Adams <adamsm.realtor@gmail.com>
2022-03-13 22:41:44 -0400

    numberChallenge

--- js-basics/numbers.js
+++ js-basics/numbers.js
@@ -1,6 +1,19 @@
 // Practice;
 
-let x = 11;
-let num = (x + 1) * 2;
+// let x = 11;
+// let num = (x + 1) * 2;
 
-console.log(num);
+// console.log(num);
+
+/************************************************************************/
+
+// let age = 27;
+// let dogYears = (age + 1) / 7;
+
+// console.log(dogYears);
+
+let studentScore = 17;
+let maxScore = 25;
+let percent = (studentScore / maxScore) * 100;
+
+console.log(percent);
