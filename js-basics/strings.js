44de7360ae55f2ce9bce9c2d62b5e451df5ebb1f
Makenzie Adams <adamsm.realtor@gmail.com>
2022-03-13 22:27:36 -0400

    stringChallenge

--- js-basics/strings.js
+++ js-basics/strings.js
@@ -1,5 +1,14 @@
-let firstName = "Makenzie";
-let lastName = "Adams";
-let fullName = firstName + " " + lastName;
+// Practice
 
-console.log(fullName);
+// let firstName = "Makenzie";
+// let lastName = "Adams";
+// let fullName = firstName + " " + lastName;
+
+// console.log(fullName);
+
+let city = "Salisbury";
+let country = "USA";
+let state = "North Carolina";
+let location = `My name is Makenzie and I live in ${city}, ${state} in the ${country}.`;
+
+console.log(location);
